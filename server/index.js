1ae6ae0a6d8022e7858de7e52f39ca1608c231b0
Tinkal300 <tinkal300@gmail.com>
2025-12-05 23:05:56 +0530

    Backend setup for vercel issue

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
-// server/server.js
-import app from "./src/app.js";
+// server/index.js
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
 
-const PORT = process.env.PORT || 5000;
+import quizRoutes from "./src/routes/quizRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("AI-Assisted Quiz API is running ✅");
 });
+
+app.use("/api/quiz", quizRoutes);
+app.use("/api/users", userRoutes);
+
+export default app; // for Vercel
+
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+  });
+}
